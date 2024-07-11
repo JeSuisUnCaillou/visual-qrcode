8e7175694191a5383eda2bc15b7aeff45fbfe0b2
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-11 14:06:39 +0200

    bundle gem

---
+++ lib/visual/qrcode.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "qrcode/version"
+
+module Visual
+  module Qrcode
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/visual/qrcode/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Visual
+  module Qrcode
+    VERSION = "0.1.0"
+  end
+end
